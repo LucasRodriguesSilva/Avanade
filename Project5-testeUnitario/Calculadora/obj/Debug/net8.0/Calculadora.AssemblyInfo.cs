52c648db6159a4a4f64285ed24717e2a150d8b44
Lucas <lucasrodrigues19021998@gmail.com>
2023-12-28 22:16:13 -0300

    Reforando o Theory usando array

--- Project5-testeUnitario/Calculadora/obj/Debug/net8.0/Calculadora.AssemblyInfo.cs
+++ Project5-testeUnitario/Calculadora/obj/Debug/net8.0/Calculadora.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculadora")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af96c1b29ba94535858ad7502215478e54676120")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12a15387acebef976364c9bed3ca9c249699508e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculadora")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculadora")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Project5-testeUnitario/CalculadoraTeste/CalculadoraTests.cs
+++ Project5-testeUnitario/CalculadoraTeste/CalculadoraTests.cs
@@ -42,15 +42,10 @@ public class CalculadoraTests
     }
 
     [Theory]
-    [InlineData(2)]
-    [InlineData(4)]
-    [InlineData(6)]
-    [InlineData(8)]
-    [InlineData(10)]
-    public void DeverVerificarSeNumerosEhPar(int numero)
+    [InlineData(new int[] { 2, 4, 6, 8, 10 })]
+    public void DeverVerificarSeNumerosEhPar(int[] numeros)
     {
-        // Act
-        bool resultado = _calc.EhPar(numero);
-
+        // Act / Assert
+        Assert.All(numeros, num => Assert.True(_calc.EhPar(num)));
     }
 }
